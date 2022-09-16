d8dd0f0a580c50c74e26881db4220b52b7ff6907
Jonathan Allen <jallen@snow.edu>
2022-09-16 09:51:00 -0600

    initial version

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import About from './Components/About';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
